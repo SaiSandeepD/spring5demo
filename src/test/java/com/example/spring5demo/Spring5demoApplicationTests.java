e778f37e1feb6cf6868d91218e6a745b5801b217
Sai Sandeep <saisandeep@Rohits-MacBook-Pro.local>
2021-07-28 15:59:36 -0400

    Initial commit

---
+++ src/test/java/com/example/spring5demo/Spring5demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring5demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
